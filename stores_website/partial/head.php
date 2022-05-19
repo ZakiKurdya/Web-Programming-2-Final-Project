3d1f1210d153320de8f82ac20a9915383593b5c7
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-05-19 19:44:26 +0300

    Restyle the website

--- stores_website/partial/head.php
+++ stores_website/partial/head.php
@@ -2,9 +2,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <title>Electro - HTML Ecommerce Template</title>
+    <title>Stores Rating Website</title>
+    <link rel="icon" href="img/icon.png">
 
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
@@ -31,5 +31,4 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
