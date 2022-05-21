3e174ff9ca0015428aca9b2fa2dcf9e750fe3a1f
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-05-22 00:27:01 +0300

    Fix delete category functionality

--- dashboard/category/delete_category.php
+++ dashboard/category/delete_category.php
@@ -2,11 +2,15 @@
 require_once "../components/DB_CONNECTION.php";
 
 $id = $_POST['id'];
+$query1 = "DELETE FROM store WHERE category_id=".$id;
+$result1 = mysqli_query($connection, $query1);
 
-$query = "DELETE FROM category WHERE id=".$id;
-$result = mysqli_query($connection,$query);
+if ($result1) {
+    $query2 = "DELETE FROM category WHERE id=".$id;
+    $result2 = mysqli_query($connection, $query2);
+}
 
-if ($result){
+if ($result2){
     header('Location:show_category.php');
 }
 
